ae4d6cee55b578cc06bde03074489e7d6e67a5dc
Swashbuckle007 <cameronbmhillis@gmail.com>
2026-05-21 15:24:38 -0400

    fix

--- js/eags-servers.js
+++ js/eags-servers.js
@@ -2,7 +2,7 @@
     var servers = [
         { addr: "wss://tuffest.org", name: "§c§ltuffnet" },
         { addr: "wss://tuff.ws", name: "§c§ltuffnet" },
-        { addr: "wss://piesmp.ca", name: "§4§lPie SMP" },
+        { addr: "wss://piesmp.ca", name: "§4§lPie SMP" }
     ];
 
     var real = Object.defineProperty;
